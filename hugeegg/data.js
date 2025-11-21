4ad42732b5dff9ef500c6e6445e67db1884eee12
adamber915 <chengmeiching1120@gmail.com>
2025-11-21 11:55:29 +0800

    2

---
+++ hugeegg/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "松山文化園區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.680168886038926,
+          "pitch": 0.09349398062596848,
+          "rotation": 11.780972450961727,
+          "target": "2-"
+        },
+        {
+          "yaw": 3.073020300069116,
+          "pitch": 0.10347902691143318,
+          "rotation": 6.283185307179586,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.43733244509996894,
+          "pitch": 0.05726319042966921,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "大巨蛋地圖與指示",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7994188894005951,
+          "pitch": 0.41127207478126415,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.0185897360944267,
+          "pitch": 0.2690736963585074,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4030266668021873,
+          "pitch": -0.017221147537888726,
+          "title": "運動主題餐廳",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "公車站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9646816358573664,
+          "pitch": 0.638589368120293,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.1859249301901134,
+          "pitch": 0.38736156615168227,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3098006946551273,
+          "pitch": -0.13702190272904602,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "棒球場地圖與餐廳指南",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.831541322335661,
+          "pitch": 0.5606780338289425,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.5219959454541163,
+          "pitch": 0.5494228312810687,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4625277353165984,
+          "pitch": -0.1806862975570631,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-youbike",
+      "name": "youbike站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4298013289287983,
+          "pitch": 0.5141769356956445,
+          "rotation": 9.42477796076938,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.547259791886928,
+          "pitch": 0.5255411512398496,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0158409030826334,
+          "pitch": -0.07168814961094938,
+          "title": "Jarana",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
