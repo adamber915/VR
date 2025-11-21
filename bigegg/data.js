d2a7fe836b8253db1a7f314f743e7962932e2027
adamber915 <chengmeiching1120@gmail.com>
2025-11-21 11:46:27 +0800

    1

---
+++ bigegg/data.js
@@ -0,0 +1,266 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "國父紀念館站",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0004505379082999639,
+          "pitch": -0.01594926900479976,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.820038432875304,
+          "pitch": 0.2126476346162267,
+          "rotation": 7.0685834705770345,
+          "target": "4-89"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8283387208666149,
+          "pitch": 0.47697272461702944,
+          "title": "國父紀念館捷運",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "外圍餐廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.014727235443819708,
+          "pitch": 0.017670259858903137,
+          "rotation": 0,
+          "target": "2-2345"
+        },
+        {
+          "yaw": -0.008836792792942916,
+          "pitch": 0.37847797759176594,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.8102770274548625,
+          "pitch": 0.08624860746532548,
+          "title": "掌門精釀啤酒餐廳",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-2345",
+      "name": "大門2345",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23639365450816285,
+          "pitch": 0.016588176226454365,
+          "rotation": 5.497787143782138,
+          "target": "3-1"
+        },
+        {
+          "yaw": -0.8316460177157303,
+          "pitch": 0.29367889351886234,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7398050709748514,
+          "pitch": 0.20930185594498063,
+          "title": "地下街",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-1",
+      "name": "大門1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.31538602452051734,
+          "pitch": -0.002816645465841816,
+          "rotation": 10.995574287564278,
+          "target": "4-89"
+        },
+        {
+          "yaw": -0.10273538054978815,
+          "pitch": 0.4017599501291649,
+          "rotation": 3.141592653589793,
+          "target": "2-2345"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4067931464641568,
+          "pitch": -0.28482985903995406,
+          "title": "棒球場",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-89",
+      "name": "大門89",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8376264913979803,
+          "pitch": 0.14435731497173698,
+          "rotation": 23.561944901923464,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.512122781245921,
+          "pitch": 0.4820818689427959,
+          "rotation": 2.356194490192345,
+          "target": "3-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.282920617571593,
+          "pitch": 0.0017831477289167452,
+          "title": "大巨蛋大廳",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
